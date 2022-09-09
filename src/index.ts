95b0c1fbf6f7251b33af13ac154fa7753b1770f0
soukainna <soukaina1lafdili@gmail.com>
2022-09-09 22:36:19 +0200

    add express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello 1")
+import  express, {Request, Response} from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send('hello');
+})
+
+app.listen(8000, () => {
+    console.log('listening to port 8000')
+})
