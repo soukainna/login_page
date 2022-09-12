ddab30a365fd1261eb4da12cb2c7e359ac5f9248
soukainna <soukaina1lafdili@gmail.com>
2022-09-12 10:18:21 +0200

    add entity folder to init users

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,34 @@
+import { log } from "console";
 import  express, {Request, Response} from "express";
+import { createConnection } from "typeorm";
 
-const app = express();
 
-app.use(express.json());
 
-app.get("/", (req: Request, res: Response) => {
-    res.send('hello');
+createConnection({
+    "type": "mysql",
+    "host": "localhost",
+    "port": 3306,
+    "username": "root",
+    "password": "root",
+    "database": "node_auth",
+    "entities": [
+        "src/entity/*.ts"
+    ],
+    "logging": false,
+    "synchronize": true
+}).then(() => {
+    console.log('connection to database')
+    const app = express();
+
+    app.use(express.json());
+
+    app.get("/", (req: Request, res: Response) => {
+        res.send('hello');
+    })
+
+    app.listen(8000, () => {
+        console.log('listening to port 8000')
+    })
+
 })
 
-app.listen(8000, () => {
-    console.log('listening to port 8000')
-})
