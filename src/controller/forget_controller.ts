b316c257639cc4904a8c2e7f341b2560830b31fc
soukainna <soukaina1lafdili@gmail.com>
2022-09-14 12:05:27 +0200

    add reset class to stock email & token

--- src/controller/forget_controller.ts
+++ src/controller/forget_controller.ts
@@ -1,8 +1,14 @@
 import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+import { Reset } from "../entity/reset_entity";
 
 export const forget =async (req: Request, res: Response) => {
     const {email} = req.body
     const token = Math.random().toString(20).substring(2, 12)
 
-    res.send(token)
+    const reset = getRepository(Reset).save({
+        email,
+        token
+    })
+    res.send(token, reset)
 } 
